08c43020223cebc4674bb8b378fdd92622db26a6
aarush-narang <aarushnarang@gmail.com>
2022-11-17 15:24:32 -0800

    Update [referrer].jsx

--- pages/[referrer].jsx
+++ pages/[referrer].jsx
@@ -3,11 +3,11 @@ import { useEffect } from "react";
 
 const linksObj = {
   github:
-    "https://aarush-narang.github.io/portfolio/?utm_source=github&utm_medium=social&utm_campaign=proj_link",
+    "https://aarush-narang.vercel.app/portfolio?utm_source=github&utm_medium=social&utm_campaign=proj_link",
   linkedin:
-    "https://aarush-narang.github.io/portfolio/?utm_source=linkedin&utm_medium=social&utm_campaign=bio",
+    "https://aarush-narang.vercel.app/portfolio?utm_source=linkedin&utm_medium=social&utm_campaign=bio",
   instagram:
-    "https://aarush-narang.github.io/portfolio/?utm_source=instagram&utm_medium=social&utm_campaign=bio",
+    "https://aarush-narang.vercel.app/portfolio?utm_source=instagram&utm_medium=social&utm_campaign=bio",
 };
 
 const LINKS = new Map(Object.entries(linksObj));
@@ -22,7 +22,7 @@ export default function Redirect({}) {
       router.replace(LINKS.get(referrer));
     } else {
       router.replace(
-        `https://aarush-narang.github.io/portfolio/?utm_source=${referrer}&utm_medium=unknown&utm_campaign=unknown`
+        `https://aarush-narang.vercel.app/portfolio?utm_source=${referrer}&utm_medium=unknown&utm_campaign=unknown`
       );
     }
   }, [referrer]);
