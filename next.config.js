f8c0fdf32dbd05115ea6fc27a53353b18219f388
aarush-narang <aarushnarang@gmail.com>
2022-11-17 15:29:53 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
-}
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/:path*",
+          has: [
+            {
+              type: "host",
+              value: "aarush-narang.vercel.app",
+            },
+          ],
+          destination: "/portfolio/:path*",
+        },
+      ],
+    };
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
