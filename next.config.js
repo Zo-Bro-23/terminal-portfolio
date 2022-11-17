dae60a2acc48268e77e366abc16801c0b1ad7c44
aarush-narang <aarushnarang@gmail.com>
2022-11-17 15:44:01 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  rewrites: async () => {
+  redirects: async () => {
     return [
       {
         source: "/:path*",
