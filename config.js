484b9367a2b73b79bb72ce991391234d049580d0
Zohan Subhash <zohan.subhash@gmail.com>
2022-12-03 15:16:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,14 @@
 const config = {
-  gtag: 'G-VNGV77LZLG',
-  prompt: 'C:\\Users\\Aarush\\Portfolio> ',
+  gtag: '',
+  prompt: 'C:\\Users\\Zo-Bro-23\\Portfolio> ',
   contact: {
-    email: 'aarushnarang@gmail.com',
-    github: 'https://github.com/aarush-narang',
-    linkedin: 'https://www.linkedin.com/in/aarush-narang',
-    instagram: 'https://www.instagram.com/aarushnarang_/',
-    host: 'https://aarush-narang.vercel.app/portfolio'
+    email: 'zohan.subhash@gmail.com',
+    github: 'https://github.com/Zo-Bro-23',
+    linkedin: 'https://www.linkedin.com/in/zobro23',
+    host: 'https://portfolio.zohan.tech/portfolio'
   },
-  source: 'https://github.com/aarush-narang/portfolio',
-  asciiArt1: `
+  source: 'https://github.com/Zo-Bro-23/terminal-portfolio',
+  asciiArt2: `
                              *       *             *         ..-. *   .    *
                            .         *   *    .-. *  .  _  _/ ^  \\   _   .   *
                                  *           /   \\     (( / ^    ^\\_/ \\       *  .
@@ -29,7 +28,7 @@ const config = {
                                                  /    |    \\
                                                 /     |     \\
                                                /      |      \\`,
-  asciiArt2: `
+  asciiArt1: `
          _____                            .__       _______                                   /\\       
         /  _  \\ _____ _______ __ __  _____|  |__    \\      \\ _____ ____________    ____    ___)/ ______
        /  /_\\  \\\\__  \\\\_  __ \\  |  \\/  ___/  |  \\   /   |   \\\\__  \\\\_  __ \\__  \\  /    \\  / ___\\/  ___/
@@ -51,7 +50,7 @@ const config = {
         `,
   welcomeMessage: 'Welcome to my Portfolio! Type "help" to see a list of commands.',
   about: ['I am 16 years old and am passionate about building beautiful, responsive websites and applications.\n', 'I have a passion for learning new technologies and constantly learning new things.\n', 'I am currently a student at Lynbrook High School.\n\n', 'You can find my projects by running the "projects" command and you can find my contact information by running the "contact" command.\n', 'I am proficient in HTML, CSS, Javascript, and Python. I have used frameworks such as React/NextJS, Flask. And I have used MongoDB in several projects as well as experimented with MySQL/SQL based databases.\n', 'I am currently working on a messaging app using NextJS and MongoDB\n'],
-  github: 'aarush-narang'
+  github: 'Zo-Bro-23'
 }
 
 export default config
