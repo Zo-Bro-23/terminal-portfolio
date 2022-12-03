f246453768511edfc77e6f03695e2037bb33b927
Zohan Subhash <zohan.subhash@gmail.com>
2022-12-03 10:42:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,55 @@
 const config = {
-  
+  gtag: 'G-VNGV77LZLG',
+  prompt: 'C:\\Users\\Aarush\\Portfolio> ',
+  contact: {
+    email: 'aarushnarang@gmail.com',
+    github: 'https://github.com/aarush-narang',
+    linkedin: 'https://www.linkedin.com/in/aarush-narang',
+    instagram: 'https://www.instagram.com/aarushnarang_/',
+    host: 'https://aarush-narang.github.io/portfolio'
+  },
+  asciiArt1: `
+                             *       *             *         ..-. *   .    *
+                           .         *   *    .-. *  .  _  _/ ^  \\   _   .   *
+                                 *           /   \\     (( / ^    ^\\_/ \\       *  .
+                                _    .   .--'\\/\\_ \\     \`/ \`-._    /  ^\\  *  .-.
+                            *  / \\_  ___/ ^     ^\\/\\'__ /  ____   /\\   /\\  _/  ^\\ *
+                              /    \\/ _/__________\\________|  |_____  / .\`/    ./^\\  .
+                         .   /\\/\\  / /______________________________\\ \\  /\\    /   \\
+                            /    \\/ /________________________________\\ \\/^ \\/\\/    ^\\
+                          /\\  .-  \\   ||___|___||||||||||||___|__|||   /^-._        ^\\
+                         /  \`-.__ ^\\  ||___|___||||||   |||___|__|||  /     \`._    _.\`\\
+                        /        \`. \\ |||||||||||||||\`  |||||||||||| /            \`    \\
+                        ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+                                                     /|\\
+                                                    / | \\
+                                                   /  |  \\
+                                                  /   |   \\
+                                                 /    |    \\
+                                                /     |     \\
+                                               /      |      \\`,
+  asciiArt2: `
+         _____                            .__       _______                                   /\\       
+        /  _  \\ _____ _______ __ __  _____|  |__    \\      \\ _____ ____________    ____    ___)/ ______
+       /  /_\\  \\\\__  \\\\_  __ \\  |  \\/  ___/  |  \\   /   |   \\\\__  \\\\_  __ \\__  \\  /    \\  / ___\\/  ___/
+      /    |    \\/ __ \\|  | \\/  |  /\\___ \\|   Y  \\ /    |    \\/ __ \\|  | \\// __ \\|   |  \\/ /_/  >___ \\ 
+      \\____|__  (____  /__|  |____//____  >___|  / \\____|__  (____  /__|  (____  /___|  /\\___  /____  >
+              \\/     \\/                 \\/     \\/          \\/     \\/           \\/     \\//_____/     \\/`,
+  asciiArt3: `
+        ___      ___           ___                  ___       ___                     ___           ___     
+       /\\  \\    /\\  \\         /\\  \\                /\\__\\     /\\  \\   ___             /\\  \\         /\\  \\    
+      /::\\  \\  /::\\  \\       /::\\  \\       ___    /:/ _/_   /::\\  \\ /\\  \\           /:/__/        /::\\  \\   
+     /:/\\:\\__\\/:/\\:\\  \\     /:/\\:\\__\\     /\\__\\  /:/ /\\__\\ /:/\\:\\  \\\\:\\  \\         /::\\  \\       /:/\\:\\  \\  
+    /:/ /:/  /:/  \\:\\  \\   /:/ /:/  /    /:/  / /:/ /:/  //:/  \\:\\  \\\\:\\  \\     ___\\/\\:\\  \\     /:/  \\:\\  \\ 
+   /:/_/:/  /:/__/ \\:\\__\\ /:/_/:/__/___ /:/__/ /:/_/:/  //:/__/ \\:\\__\\\\:\\  \\   /\\__\\  \\:\\  \\   /:/__/ \\:\\__\\
+   \\:\\/:/  /\\:\\  \\ /:/  / \\:\\/:::::/  //::\\  \\ \\:\\/:/  / \\:\\  \\ /:/  / \\:\\  \\ /:/  /   \\:\\  \\__\\:\\  \\ /:/  /
+    \\::/__/  \\:\\  /:/  /   \\::/~~/~~~~/:/\\:\\  \\ \\::/__/   \\:\\  /:/  /   \\:\\  /:/  /     \\:\\/\\__\\\\:\\  /:/  / 
+     \\:\\  \\   \\:\\/:/  /     \\:\\~~\\    \\/__\\:\\  \\ \\:\\  \\    \\:\\/:/  /     \\:\\/:/  /       \\::/  / \\:\\/:/  /  
+      \\:\\__\\   \\::/  /       \\:\\__\\        \\:\\__\\ \\:\\__\\    \\::/  /       \\::/  /        /:/  /   \\::/  /   
+       \\/__/    \\/__/         \\/__/         \\/__/  \\/__/     \\/__/         \\/__/         \\/__/     \\/__/    
+        `,
+  welcomeMessage: 'Welcome to my Portfolio! Type "help" to see a list of commands.',
+  about: ['I am 16 years old and am passionate about building beautiful, responsive websites and applications.\n', 'I have a passion for learning new technologies and constantly learning new things.\n', 'I am currently a student at Lynbrook High School.\n\n', 'You can find my projects by running the "projects" command and you can find my contact information by running the "contact" command.\n', 'I am proficient in HTML, CSS, Javascript, and Python. I have used frameworks such as React/NextJS, Flask. And I have used MongoDB in several projects as well as experimented with MySQL/SQL based databases.\n', 'I am currently working on a messaging app using NextJS and MongoDB\n']
 }
 
 export default config
